d419f23bf9db9c93c06a0130de4239592c064e29
AndreFra96 <brioschiandrea@tiscali.it>
2020-10-29 14:17:37 +0100

    SistemaAstronomico completo con simulazione

--- Tester.java
+++ Tester.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.TreeSet;
 
 public class Tester {
     public static void main(String[] args){
