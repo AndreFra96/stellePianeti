da3688224ad754afa081718b650f520fe5e6e8c1
AndreFra96 <brioschiandrea@tiscali.it>
2020-10-29 11:20:29 +0100

    Aggiunte specifiche

--- Punto.java
+++ Punto.java
@@ -10,24 +10,43 @@ public class Punto {
     private final int y;
     private final int z;
 
+    /**
+     * Post-condizioni: Restituisce un nuovo punto
+     */
     public Punto(final int x, final int y, final int z) {
-        this.x = Objects.requireNonNull(x);
-        this.y = Objects.requireNonNull(y);
-        this.z = Objects.requireNonNull(z);
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
+    /**
+     * Post-condizioni: Restituisce un nuovo punto, corrispondente al punto ottenuto
+     * sommando le coordinate del punto attuale con quelle del punto in input
+     * <p>
+     * lancia NullPointerException se il punto in input è null
+     */
     public Punto somma(Punto nuovoPunto) {
         Objects.requireNonNull(nuovoPunto);
         return new Punto(this.getX() + nuovoPunto.getX(), this.getY() + nuovoPunto.getY(),
                 this.getY() + nuovoPunto.getY());
     }
 
+    /**
+     * Post-condizioni: Restituisce un nuovo punto, corrispondente al punto ottenuto
+     * sottraendo le coordinate del punto in input a quelle del punto attuale
+     * <p>
+     * lancia NullPointerException se il punto in input è null
+     */
     public Punto sottrazione(Punto nuovoPunto) {
         Objects.requireNonNull(nuovoPunto);
         return new Punto(this.getX() - nuovoPunto.getX(), this.getY() - nuovoPunto.getY(),
                 this.getY() - nuovoPunto.getY());
     }
 
+    /**
+     * Post-condizioni: Restituisce un intero corrispondente alla norma del Punto
+     * attuale
+     */
     public int norma() {
         int norma = this.getX() > 0 ? this.getX() : -this.getX();
         norma += this.getY() > 0 ? this.getY() : -this.getY();
@@ -35,28 +54,53 @@ public class Punto {
         return norma;
     }
 
+    /**
+     * Post-condizioni: restituisce un intero corrispondente alla coordinata x del
+     * punto attuale
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Post-condizioni: restituisce un intero corrispondente alla coordinata y del
+     * punto attuale
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * Post-condizioni: restituisce un intero corrispondente alla coordinata z del
+     * punto attuale
+     */
     public int getZ() {
         return z;
     }
 
+    /**
+     * Post-condizioni: restituisce una rappresentazione del punto composta in
+     * questo modo: (x, y, z)
+     */
     @Override
     public String toString() {
         return "(" + this.getX() + ", " + this.getY() + ", " + this.getZ() + ")";
     }
 
+    /**
+     * Due punti hanno lo stesso hashcode quando il loro toString() è identico,
+     * ovvero quando hanno x,y e z uguali Post-condizioni: restituisce un intero
+     * corrispondente all'hashCode del toString() del punto
+     */
     @Override
     public int hashCode() {
         return this.toString().hashCode();
     }
 
+    /**
+     * Due punti sono uguali quando hanno tutte le stesse coordinate
+     * Post-condizioni: Restituisce true se i due punti hanno le stesse coordinate
+     */
     @Override
     public boolean equals(Object obj) {
         if (!(obj instanceof Punto))
