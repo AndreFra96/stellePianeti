e3b22176ddb11064fc0aaae4d6e17e1b4b88721c
AndreFra96 <brioschiandrea@tiscali.it>
2020-10-29 11:07:39 +0100

    Creazione oggetto Punto

---
+++ Punto.java
@@ -0,0 +1,54 @@
+package stellePianeti;
+
+import java.util.Objects;
+
+/**
+ * Punto rappresenta un punto nello spazio
+ */
+public class Punto {
+    private final int x;
+    private final int y;
+    private final int z;
+
+    public Punto(final int x, final int y, final int z) {
+        this.x = Objects.requireNonNull(x);
+        this.y = Objects.requireNonNull(y);
+        this.z = Objects.requireNonNull(z);
+    }
+
+    public Punto somma(Punto nuovoPunto) {
+        Objects.requireNonNull(nuovoPunto);
+        return new Punto(this.getX() + nuovoPunto.getX(), this.getY() + nuovoPunto.getY(),
+                this.getY() + nuovoPunto.getY());
+    }
+
+    public Punto sottrazione(Punto nuovoPunto) {
+        Objects.requireNonNull(nuovoPunto);
+        return new Punto(this.getX() - nuovoPunto.getX(), this.getY() - nuovoPunto.getY(),
+                this.getY() - nuovoPunto.getY());
+    }
+
+    public int norma() {
+        int norma = this.getX() > 0 ? this.getX() : -this.getX();
+        norma += this.getY() > 0 ? this.getY() : -this.getY();
+        norma += this.getZ() > 0 ? this.getZ() : -this.getZ();
+        return norma;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    @Override
+    public String toString() {
+        return "("+this.getX()+", "+this.getY()+", "+this.getZ()+")";
+    }
+}
